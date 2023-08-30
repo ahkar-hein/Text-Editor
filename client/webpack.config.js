c7c75a8eb06ef9b6784edb625c89cea65e83889f
Ahkar Hein <ahkarhein04@gmail.com>
2023-08-29 21:17:28 -0700

    remove bable

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -52,17 +52,6 @@ module.exports = () => {
           test: /\.css$/,
           use: ['style-loader', 'css-loader'],
         },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-            },
-          },
-        },
       ],
     },
   };
